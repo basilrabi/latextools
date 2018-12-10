24d2895a992b782b71f47d695574a45342af7cc6
basilrabi <ericbasil.rabi@gmail.com>
2018-12-10 18:14:09 +0800

    add an option to print fix decimal digits in percent

--- R/percent.R
+++ R/percent.R
@@ -3,13 +3,14 @@
 #' Converts a numeric to a percent format
 #' @param x a number
 #' @param latex logical value indicating if output is in latex
+#' @param digits number of decimal digits to be used
 #' @return a character string
 #' @export percent
 #' @importFrom dplyr "%>%"
-percent <- function(x, latex = TRUE) {
+percent <- function(x, latex = TRUE, digits = 2) {
   sep <- " "
   if (latex)
     sep <- "~"
 
-  ifelse(is.na(x), NA, (round(x * 100, digits = 2) %>% paste0(sep ,"\\%")))
+  ifelse(is.na(x), NA, (asDec(x * 100, digits = digits) %>% paste0(sep ,"\\%")))
 }
--- tests/testthat/test_percent.R
+++ tests/testthat/test_percent.R
@@ -1,8 +1,9 @@
 library(latextools)
 
 test_that("percent works",{
-  expect_equal(percent(1), "100~\\%")
-  expect_equal(percent(0.5), "50~\\%")
+  expect_equal(percent(1), "100.00~\\%")
+  expect_equal(percent(1, digits = 0), "100~\\%")
+  expect_equal(percent(0.5), "50.00~\\%")
   expect_equal(percent(0.1052), "10.52~\\%")
   expect_equal(percent(NA), NA)
 })
