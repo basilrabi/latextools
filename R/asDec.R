23ff22aff7891cb7d97b38bc890f79f1615c9fd1
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2020-06-23 15:50:25 +0800

    print blank for NA values and accomodate matrix

--- R/asDec.R
+++ R/asDec.R
@@ -1,9 +1,9 @@
-#' Convert to formated decimal
+#' Convert to formatted decimal
 #'
 #' Converts a numeric into pretty decimals
-#' @param x a numeric vector
+#' @param x a numeric vector or matrix
 #' @param digits number of decimal digits to be used
-#' @return a character vector of formatted decimals
+#' @return a character vector or matrix of formatted decimals
 #' @export asDec
 asDec <- function(x, digits = 2L) {
   out <- round(x = x, digits = digits)
@@ -13,6 +13,21 @@ asDec <- function(x, digits = 2L) {
                 nsmall         = digits,
                 small.interval = 3L,
                 small.mark     = " ")
+
+  if (is.matrix(x)) {
+    col_names <- colnames(x)
+    row_names <- rownames(x)
+    out <- as.vector(out)
+  }
+
   out <- ifelse(grepl(pattern = "NA", out), "", out)
-  out
+
+  if (is.matrix(x)) {
+    out <- matrix(data = out,
+                  nrow = length(row_names),
+                  ncol = length(col_names),
+                  dimnames = list(row_names, col_names))
+  }
+
+  return(out)
 }
